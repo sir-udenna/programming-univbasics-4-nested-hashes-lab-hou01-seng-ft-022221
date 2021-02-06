163692faacabc2107c3e98020ad245cbdcd2d0c7
Udenna Nebeolisa <udennaboy@gmail.com>
2021-02-06 09:27:18 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,8 +15,9 @@ def hopper
         :languages => ["C"]
       }
     }
-
+    return programmer_hash[:grace_hopper]
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -36,7 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    return programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -81,7 +83,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+    programmer_hash = {
+      :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP","C"]
+      }
+    }
 end
 
 def changing_alan
@@ -103,7 +110,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    return programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +133,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+    return programmer_hash
 
 end
